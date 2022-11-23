835312cc02c852d0161d5068498a9679b9f4fb3b
jesusd2003 <102818263+jesusd2003@users.noreply.github.com>
2022-11-22 21:21:21 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function ing(){
+    var uso;
+    var con;
+    
+  uso=document.getElementById("uso").value;
+  con=document.getElementById("con").value;
+  
+    if(uso==""||con==""){
+       alert("campos obligatorios");
+       }
+    
+   }  
